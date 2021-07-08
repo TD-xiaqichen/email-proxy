b141e1ff2c7f69d24f5fd2e083d5d5e0bf7d0a6c
TD-xiaqichen <qichen.xia@china.talkdesk.com>
2021-07-08 17:02:11 +0800

    javamail test successful

--- src/main/java/com/td/EmailproxyApplication.java
+++ src/main/java/com/td/EmailproxyApplication.java
@@ -1,9 +1,11 @@
 package com.td;
 
+import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEmailTools
 public class EmailproxyApplication {
 
 	public static void main(String[] args) {
